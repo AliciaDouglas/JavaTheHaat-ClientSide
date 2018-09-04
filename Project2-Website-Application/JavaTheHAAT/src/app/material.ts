9b1dd87d8c7ae270e0bef5d0518685ce67728219
AHudson94 <jamalhudson80@yahoo.com>
2018-09-04 15:12:08 -0400

    Updated website

--- Project2-Website-Application/JavaTheHAAT/src/app/material.ts
+++ Project2-Website-Application/JavaTheHAAT/src/app/material.ts
@@ -1,9 +1,28 @@
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 import { NgModule } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatStepperModule} from '@angular/material/stepper';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule],
-    exports: [MatButtonModule, MatCheckboxModule],
+    imports: [MatButtonModule,
+              MatCheckboxModule,
+              MatGridListModule,
+              MatStepperModule,
+              FormsModule,
+              ReactiveFormsModule,
+              MatFormFieldModule,
+              MatInputModule
+            ],
+    exports: [MatButtonModule,
+              MatCheckboxModule,
+              MatGridListModule,
+              MatStepperModule,
+              FormsModule,
+              ReactiveFormsModule,
+              MatFormFieldModule,
+              MatInputModule
+            ],
 })
 export class Material { }
