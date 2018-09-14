59c0d44927c262c9ecccb091af3b82eaaa1e8476
AHudson94 <jamalhudson80@yahoo.com>
2018-09-14 19:50:00 -0400

    Updated with the homescreen view post

--- Project2-Website-Application/JavaTheHAAT/src/app/pages/home-screen/home-screen.component.ts
+++ Project2-Website-Application/JavaTheHAAT/src/app/pages/home-screen/home-screen.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Posts } from 'src/app/models/posts';
+import { HttpClient } from '@angular/common/http';
+import { UsersService } from 'src/app/services/users.service';
+import { SafePipe } from 'src/app/pipes/safe.pipe';
 
 @Component({
   selector: 'app-home-screen',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-screen.component.css']
 })
 export class HomeScreenComponent implements OnInit {
+  posts: Posts[];
 
-  constructor() { }
+  constructor(private http: HttpClient, private userService: UsersService, private safePipe: SafePipe) { }
 
   ngOnInit() {
+    this.getAllPosts();
+    for (let i = 0; i < this.posts.length; i++) {
+      this.posts[i].video = this.safePipe.transform(this.posts[i].video);
+    }
+  }
+
+  // This method will get all posts... It hould be changed to getAllPostsByUid, so a user can only see their posts
+  getAllPosts() {
+    this.userService.getAllPosts().subscribe(result => {
+      console.log('This is the JSON title of first:' + result[0].title);
+      this.posts = result;
+    });
   }
 
 }
