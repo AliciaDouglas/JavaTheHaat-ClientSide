7c301457bde2e9a49252cc9a6a8f59c2c1063221
Tyler <tyler.d.atkinson@gmail.com>
2018-09-17 10:10:56 -0400

    add environment var for amazon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@
 export const environment = {
   production: true,
   envName: `prod`,
-  context: `http://localhost:4200`
+  context: `http://localhost:4200`,
+  apiUrl: 'http://http://ec2-18-223-33-87.us-east-2.compute.amazonaws.com:8080/'
 };
